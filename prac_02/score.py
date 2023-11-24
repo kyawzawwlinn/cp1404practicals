006974effe563ca4b1df59eb02cc771127226974
kyawzawwlinn <kyawzawwlinn@my.jcu.edu.au>
2023-11-24 21:43:11 +0800

    after adding functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,16 +1,21 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+import random
 
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-else:
-    if score >= 90:
-        print("Excellent")
+def determine_score_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
     elif score >= 50:
-        print("Passable")
+        return "Passable"
     else:
-        print("Bad")
+        return "Bad"
+
+
+user_score = float(input("Enter score: "))
+user_result = determine_score_status(user_score)
+print(user_result)
+
+random_score = random.uniform(0, 100)
+random_result = determine_score_status(random_score)
+print(f"Random Score Result: {random_result}")
