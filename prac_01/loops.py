4aa1c56fca028eced3425bf53c4deb43cd55c877
kyawzawwlinn <kyawzawwlinn@my.jcu.edu.au>
2023-11-24 21:10:19 +0800

    Hi

---
+++ prac_01/loops.py
@@ -0,0 +1,23 @@
+for i in range(1, 21, 2):
+    print(i, end=' ')
+print()
+
+# question a
+for i in range(0, 101, 10):
+    print(i, end=' ')
+print()
+
+# question b
+for i in range(20, 0, -1):
+    print(i, end=' ')
+print()
+
+# question c
+number_of_stars = int(input("Number of stars: "))
+for i in range(number_of_stars):
+    print("*", end=' ')
+print()
+
+# question d
+for i in range(1, number_of_stars + 1):
+    print("*" * i)
