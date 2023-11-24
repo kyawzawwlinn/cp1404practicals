56a7efee2c12672e7195c92fc52b60737d95b2f1
kyawzawwlinn <kyawzawwlinn@my.jcu.edu.au>
2023-11-24 21:37:20 +0800

    after modifying and adding two functions

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,24 +1,29 @@
-"""
-CP1404/CP5632 - Practical
-Pseudocode for temperature conversion
-"""
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9.0 / 5 + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return 5 / 9 * (fahrenheit - 32)
 
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
+
 print(MENU)
 choice = input(">>> ").upper()
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
+        result = celsius_to_fahrenheit(celsius)
+        print(f"Result: {result:.2f} F")
     elif choice == "F":
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f"Result: {celsius:.2f} C")
+        result = fahrenheit_to_celsius(fahrenheit)
+        print(f"Result: {result:.2f} C")
     else:
         print("Invalid option")
+
     print(MENU)
     choice = input(">>> ").upper()
+
 print("Thank you.")
