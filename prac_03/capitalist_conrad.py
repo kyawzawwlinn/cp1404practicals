ed3329d6dbf1f93a236a3cb21b311b7af116f0d9
kyawzawwlinn <kyawzawwlinn@my.jcu.edu.au>
2023-11-27 21:07:09 +0800

    Changed some constant names

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -1,9 +1,9 @@
 import random
 
-MAX_INCREASE = 0.175  # 17.5%
-MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 1.0
-MAX_PRICE = 100.0
+MAXIMUM_INCREASE = 0.175  # 17.5%
+MAXIMUM_DECREASE = 0.05  # 5%
+MINIMUM_PRICE = 1.0
+MAXIMUM_PRICE = 100.0
 INITIAL_PRICE = 10.0
 OUTPUT_FILE = "stock_simulation_output.txt"
 
@@ -16,14 +16,14 @@ out_file = open(OUTPUT_FILE, 'w')
 print(f"Starting price: ${price:,.2f}")
 print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
 
-while MIN_PRICE <= price <= MAX_PRICE:
+while MINIMUM_PRICE <= price <= MAXIMUM_PRICE:
     price_change = 0
 
     if random.randint(1, 2) == 1:
 
-        price_change = random.uniform(0, MAX_INCREASE)
+        price_change = random.uniform(0, MAXIMUM_INCREASE)
     else:
-        price_change = random.uniform(-MAX_DECREASE, 0)
+        price_change = random.uniform(-MAXIMUM_DECREASE, 0)
 
     price *= (1 + price_change)
     number_of_days += 1
