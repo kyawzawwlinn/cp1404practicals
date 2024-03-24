7f8b50c86ccb2707f2c8e8fcf1db6cd086ccc172
kyawzawwlinn <kyawzawwlinn@my.jcu.edu.au>
2024-03-24 13:45:11 +0800

    final codes

--- prac_01/a.py
+++ prac_01/a.py
@@ -1,2 +1,9 @@
-x = 5 // 2
-print(x)
+def print_hills(num, symbol1, symbol2):
+    for i in range(1, num + 1):
+        print(''.join([symbol1 if j % 2 == 0 else symbol2 for j in range(i)]))
+    for i in range(num - 1, 0, -1):
+        print(''.join([symbol1 if j % 2 == 0 else symbol2 for j in range(i)]))
+
+
+# Example usage:
+print_hills(4, "H", "!")
